f4a9bc1ad7ec2e6be84631026073af7c9065102b
Alexey <leventcov.a.v@gmail.com>
2022-12-20 17:32:58 +0300

    First

---
+++ src/services/MarvelService.js
@@ -0,0 +1,38 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'cad03cf6d6b1867a1fd3d175acab14c6'
+    getResourse = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+        return await res.json();
+
+    };
+
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}/characters?limit=9&offset=210&apikey=${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}/characters/${id}?&apikey=${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        let description = char.description ? char.description : 'нет описания'
+        description = description.length > 170 ? description.slice(0, 170)+'...' : description
+        return {
+            name: char.name,
+            description: description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+
+
+}
+
+export default MarvelService;
